bacff7c68c772418c3795211bc23ba0f213d5b10
Conganh97 <anhbc.afsystem@gmail.com>
2022-05-24 08:55:38 +0700

    upbt

---
+++ MotelManager/src/SortPersonByID.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+public class SortPersonByID implements Comparator<Person> {
+    @Override
+        public int compare(Person o1, Person o2){
+            if (o1.getID().compareTo(o2.getID()) > 0 ) return 1;
+            else return -1;
+        }
+}
+
+
